d116c75d4e02ad9648b975c24a3da41a80b4d52d
Nikita Radzevicius <nradzevicius10@gmail.com>
2024-09-15 18:25:54 +0100

    :zap: actual commit from last time

---
+++ scripts/ui.js
@@ -0,0 +1,32 @@
+let totalMoney = document.getElementById('totalValue'); // Total Money owned
+let betInput = document.getElementById('moneyInput'); // Betting value inputted
+
+let allinBtn = document.getElementById('allInBtn');
+let doubleBtn = document.getElementById('doubleBtn');
+let halfBtn = document.getElementById('halfBtn');
+
+// Set bet input default to $100
+betInput.value = 100;
+
+allinBtn.addEventListener("click", function() {
+    betInput.value = totalMoney.innerText.substring(1);
+})
+
+doubleBtn.addEventListener("click", function() {
+    if (betInput.value*2 < totalMoney.innerText.substring(1)) {
+        betInput.value *= 2;
+    } else {
+        betInput.value = totalMoney.innerText.substring(1);
+    }
+});
+
+halfBtn.addEventListener("click", function() {
+    if (betInput.value*1 > totalMoney.innerText.substring(1)) {
+        betInput.value = totalMoney.innerText.substring(1);
+    }
+    else if (betInput.value/2 < 1) {
+        betInput.value = 1
+    } else {
+        betInput.value /= 2;
+    }
+});
