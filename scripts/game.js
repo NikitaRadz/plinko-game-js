ffe567b40eca49a72500d6202411b5703994c8e0
Nikita Radzevicius <nradzevicius10@gmail.com>
2024-09-15 22:06:01 +0100

    :zap: added new space for game screen

---
+++ scripts/game.js
@@ -0,0 +1,14 @@
+// const engine = Matter.Engine.create();
+// const render = Matter.Render.create({
+//     element: document.body,
+//     engine:engine
+// });
+// let ground = Matter.Bodies.rectangle(400,600,810,60, {isStatic:true});
+// let ballA = Matter.Bodies.circle(400,200,30);
+// let ballB = Matter.Bodies.circle(500,200,30);
+
+// Matter.World.add(engine.world,[ballA,ballB,ground]);
+// Matter.Engine.run(engine);
+// Matter.Render.run(render);
+
+
