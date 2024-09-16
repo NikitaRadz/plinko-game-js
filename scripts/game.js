6ac759f56a41d228f53bfa865e4a7ee03fd0c59f
Nikita Radzevicius <nradzevicius10@gmail.com>
2024-09-16 20:33:07 +0100

    :zap: managed to get game screen to actually render onto the canvas

--- scripts/game.js
+++ scripts/game.js
@@ -1,14 +1,17 @@
-// const engine = Matter.Engine.create();
-// const render = Matter.Render.create({
-//     element: document.body,
-//     engine:engine
-// });
-// let ground = Matter.Bodies.rectangle(400,600,810,60, {isStatic:true});
-// let ballA = Matter.Bodies.circle(400,200,30);
-// let ballB = Matter.Bodies.circle(500,200,30);
+const engine = Matter.Engine.create();
+const render = Matter.Render.create({
+    engine:engine,
+    canvas: document.querySelector('#gameCanvas')
+});
 
-// Matter.World.add(engine.world,[ballA,ballB,ground]);
-// Matter.Engine.run(engine);
-// Matter.Render.run(render);
+// Elements in game
+let ground = Matter.Bodies.rectangle(400,600,1000,100, {isStatic:true});
+let ballA = Matter.Bodies.circle(400,200,30);
+let ballB = Matter.Bodies.circle(500,200,30);
+
+// Runs the actual game
+Matter.World.add(engine.world,[ballA,ballB,ground]);
+Matter.Engine.run(engine); // Use instead of Engine.run I guess?
+Matter.Render.run(render);
 
 
