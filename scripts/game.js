1dcad927f3b77358e91e7acf9d36839a7d48d64f
Nikita Radzevicius <nradzevicius10@gmail.com>
2024-09-16 23:07:33 +0100

    :construction: trying to make game canvas properly resize on displayport change

--- scripts/game.js
+++ scripts/game.js
@@ -1,17 +1,60 @@
-const engine = Matter.Engine.create();
-const render = Matter.Render.create({
-    engine:engine,
-    canvas: document.querySelector('#gameCanvas')
+const gameCanvas = document.getElementById('gameCanvas');
+let gameWidth = gameCanvas.clientWidth;
+let gameHeight = gameCanvas.clientHeight;
+
+// module aliases
+let Engine = Matter.Engine,
+    Render = Matter.Render,
+    Runner = Matter.Runner,
+    Bodies = Matter.Bodies,
+    Composite = Matter.Composite,
+    World = Matter.World;
+
+// Create an engine
+let engine = Engine.create();
+
+// Create a renderer
+let render = Render.create({
+    element: gameCanvas,
+    engine: engine,
+    options: {
+        width: gameWidth,
+        height: gameHeight,
+        background: "transparent",
+        wireframes: true,
+        showAngleIndicator: true
+    }
 });
 
-// Elements in game
-let ground = Matter.Bodies.rectangle(400,600,1000,100, {isStatic:true});
-let ballA = Matter.Bodies.circle(400,200,30);
-let ballB = Matter.Bodies.circle(500,200,30);
+// Create the game "world"
+let world = engine.world;
 
-// Runs the actual game
-Matter.World.add(engine.world,[ballA,ballB,ground]);
-Matter.Engine.run(engine); // Use instead of Engine.run I guess?
-Matter.Render.run(render);
+// Objects in game world
+let circleA = Bodies.circle(500 ,500 ,30);
+let leftWall = Bodies.rectangle(gameWidth/4 ,
+    gameHeight/2,
+    5,
+    gameHeight*0.7, 
+    {isStatic:true});
+let rightWall = Bodies.rectangle((gameWidth/4)*3,
+    gameHeight/2,
+    5,
+    gameHeight*0.7,
+    {isStatic:true});
 
+// Spawns all objects in world
+World.add(world,[circleA,leftWall,rightWall]);
 
+// Runs both engine and the renderer
+Runner.run(engine);
+Render.run(render);
+
+function handleResize(gameCanvas) {
+    // set game canvas to new values according to change in screen size
+    render.canvas.width = gameCanvas.clientWidth;
+    render.canvas.height = gameCanvas.clientHeight;
+
+    // Change position of walls according to resize as well below
+}
+
+window.addEventListener("resize", () => handleResize(gameCanvas));
