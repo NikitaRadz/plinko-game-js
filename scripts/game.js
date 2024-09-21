77f304667e6d384fc4f3affaa11754ec799e03c0
Nikita Radzevicius <nradzevicius10@gmail.com>
2024-09-21 21:17:04 +0100

    :sparkles: added new 'methods.js' file

--- scripts/game.js
+++ scripts/game.js
@@ -65,80 +65,9 @@ World.add(world,[leftWall,rightWall,ground, pegPyramid]);
 Runner.run(engine);
 Render.run(render);
 
-function handleResize(gameCanvas) {
-    // set game canvas to new values according to change in screen size
-    render.canvas.width = gameCanvas.clientWidth;
-    render.canvas.height = gameCanvas.clientHeight;
-
-    Matter.Body.setPosition(
-        leftWall,
-        Matter.Vector.create(
-            (gameCanvas.clientWidth/2)-270,
-            gameCanvas.clientHeight/2,
-        )
-    );
-    Matter.Body.setPosition(
-        rightWall,
-        Matter.Vector.create(
-            (gameCanvas.clientWidth/2)+270,
-            gameCanvas.clientHeight/2
-        )
-    );
-    Matter.Body.setPosition(
-        ground,
-        Matter.Vector.create(
-            gameCanvas.clientWidth/2,
-            (gameCanvas.clientHeight/2)+200
-        )
-    );
-
-    let newPyramidVector = Matter.Vector.create((gameCanvas.clientWidth/2)+15, (gameCanvas.clientHeight/2)-150);
-    let pyramidVectorDelta = Matter.Vector.sub(newPyramidVector, oldPyramidVector);
-    oldPyramidVector = newPyramidVector;
-    Matter.Composite.translate(pegPyramid, pyramidVectorDelta);
-}
-
 window.addEventListener("resize", () => handleResize(gameCanvas));
-
-function spawnBall() {
-    let offSet = Math.random()*5;
-    let newBall = Bodies.circle(
-        (gameCanvas.clientWidth/2)+offSet,offSet,10,{restitution:0.1}
-    );
-    World.add(world, newBall);
-}
-
-function createPyramid(composite,rows,radius,gap,startX,startY) {
-    // Loop through each row creating the circles
-    for (let currentRow = 0; currentRow < rows; currentRow++) {
-
-        let circlesInRow = rows - currentRow;
-
-        // Calculate the x at the centre of the pyramid
-        let centreX = startX - circlesInRow * (radius + gap/2);
-
-        // Create each circle in the row
-        for (let circ = 0; circ < circlesInRow; circ++) {
-            let x = centreX + circ * ((radius * 2) + gap) + radius;
-            let y = startY + currentRow * ((radius * 2) + gap);
-            let circle = Bodies.circle(x, y, radius, {isStatic:true});
-            Matter.Composite.add(composite, circle);
-        }
-    }
-    
-    // Removes top circle from pyramid so ball can spawn there
-    for (let layer = 1; layer < 4; layer++) {
-        Matter.Composite.remove(pegPyramid, pegPyramid.bodies[pegPyramid.bodies.length-1]);
-    }
-    
-    // Makes the pyramid the right way up
-    let rotationVector = Matter.Vector.create(gameCanvas.clientWidth/2,gameCanvas.clientHeight/2);
-    Matter.Composite.rotate(pegPyramid, Math.PI, rotationVector);
-}
-
 
 /**
  * TODO:
- * - Create balls that can drop
  * - Create ground that the balls can dorp into and be deleted once they hit the multiplier
  */
